d7addb13be39092944ef8a073a60a0c7cdcc1b60
KEENTO <handball0321@gmail.com>
2022-05-08 13:47:49 -0700

    Add dnd feature

--- src/components/List/TaskList/TaskList.js
+++ src/components/List/TaskList/TaskList.js
@@ -9,6 +9,7 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import ContainerUI from "../../UI/ContainerUI";
 import { styled } from "@mui/system";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const UlComponent = styled("ul")({
   paddingLeft: 0,
@@ -23,7 +24,7 @@ const TaskList = (props) => {
   const tasks = useSelector((state) => state.list.tasks);
   const totalQuantityOfTasks = useSelector((state) => state.list.totalQuantity);
 
-  const [allTask, setAllTask] = useState(tasks);
+  // const [allTask, setAllTask] = useState(tasks);
   const [editTaskId, setEditTaskId] = useState();
   const [modalDefaultTaskText, setModalDefaultTaskText] = useState("");
   const [modalDefaultImportance, setModalDefaultImportance] = useState("");
@@ -49,31 +50,76 @@ const TaskList = (props) => {
     dispatch(listActions.removeTaskFromList(id));
   };
 
-  useEffect(() => {
-    setAllTask(
-      tasks.map((task, index) => {
-        return (
-          <TaskItem
-            key={index}
-            index={index}
-            id={task.id}
-            taskText={task.taskText}
-            dueDate={task.dueDate}
-            importance={task.importance}
-            onStartEditing={() =>
-              startEditingHandler(
-                task.taskText,
-                index,
-                task.importance,
-                task.id
-              )
-            }
-            onRemoveItem={removeItemHandler.bind(null, task.id)}
-          />
-        );
-      })
-    );
-  }, [tasks]);
+  // useEffect(() => {
+  //   console.log("somehow, rendering !!");
+  //   setAllTask(
+  //     tasks.map((task, index) => {
+  //       return (
+  //         <TaskItem
+  //           key={index}
+  //           index={index}
+  //           id={task.id}
+  //           taskText={task.taskText}
+  //           dueDate={task.dueDate}
+  //           importance={task.importance}
+  //           onStartEditing={() =>
+  //             startEditingHandler(
+  //               task.taskText,
+  //               index,
+  //               task.importance,
+  //               task.id
+  //             )
+  //           }
+  //           onRemoveItem={removeItemHandler.bind(null, task.id)}
+  //         />
+  //       );
+  //     })
+  //   );
+  // }, [tasks]);
+
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
+
+    if (!destination) return;
+
+    // if (
+    //   destination.droppableId === source.droppableId
+    //   &&
+    //   destination.droppableIndex === source.droppableIndex
+    // )
+    //   return;
+
+    let add,
+      active = tasks;
+    if (
+      source.droppableId === "TodoList" &&
+      destination.droppableId === "TodoList"
+    ) {
+      // test
+      console.log(active);
+      dispatch(
+        listActions.manipulateTasksOrder({
+          add: active[source.index],
+          active: active,
+          sourceIndex: source.index,
+          destinationIndex: destination.index,
+        })
+      );
+      // add = active[source.index];
+      // console.log(add);
+      // active.splice(source.index, 1);
+      // active.splice(destination.index, 0, add);
+    }
+
+    const valObj = {
+      add: active[source.index],
+      active: tasks,
+      sourceIndex: source.index,
+      destinationIndex: destination.index,
+    };
+
+    // setAllTask(active);
+  };
 
   return (
     <ContainerUI>
@@ -97,22 +143,58 @@ const TaskList = (props) => {
             defaultImportance={modalDefaultImportance}
           />
         )}
-        <ContainerUI>
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <Typography
-              variant="h5"
-              component="h2"
-              color="text.primary"
-              paddingRight={4}
-            >
-              Task List
-            </Typography>
-            <Typography variant="body1" component="span" color="text.primary">
-              {totalQuantityOfTasks} tasks left.
-            </Typography>
-          </Box>
-          <UlComponent>{allTask}</UlComponent>
-        </ContainerUI>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <ContainerUI>
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <Typography
+                variant="h5"
+                component="h2"
+                color="text.primary"
+                paddingRight={4}
+              >
+                Task List
+              </Typography>
+              <Typography variant="body1" component="span" color="text.primary">
+                {totalQuantityOfTasks} tasks left.
+              </Typography>
+            </Box>
+            <UlComponent>
+              <Droppable droppableId="TodoList">
+                {(provided) => (
+                  <div
+                    className="todos"
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                  >
+                    {/* temporary */}
+                    {tasks.map((task, index) => {
+                      return (
+                        <TaskItem
+                          key={task.id}
+                          index={index}
+                          id={task.id}
+                          taskText={task.taskText}
+                          dueDate={task.dueDate}
+                          importance={task.importance}
+                          onStartEditing={() =>
+                            startEditingHandler(
+                              task.taskText,
+                              index,
+                              task.importance,
+                              task.id
+                            )
+                          }
+                          onRemoveItem={removeItemHandler.bind(null, task.id)}
+                        />
+                      );
+                    })}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </UlComponent>
+          </ContainerUI>
+        </DragDropContext>
       </Box>
     </ContainerUI>
   );
